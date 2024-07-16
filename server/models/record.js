15773782acb2d294d8951e97798400596891a937
vyiirus11 <dangh@ufl.edu>
2024-07-15 20:35:39 -0400

    Reapply "auth progress"
    This reverts commit 62799bfa82ef869ad0435d9011a477d8119d34d6.

--- server/models/record.js
+++ server/models/record.js
@@ -4,6 +4,7 @@ const recrodSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Record from "../models/record.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
 const router = express.Router();
 
