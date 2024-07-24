770e1502bfe29b43bd947b765295563a74553f58
lucas <146390150+lucasrodr1010@users.noreply.github.com>
2024-07-24 18:59:47 -0400

    Redirecting changes post login

--- client/src/components/Panel/AccountPanel.jsx
+++ client/src/components/Panel/AccountPanel.jsx
@@ -4,6 +4,7 @@ import {
   Box,
   Button,
   Heading,
+  Center,
   Table,
   Tbody,
   Td,
@@ -16,5 +17,30 @@ import { API_URL } from "../../config";
 
 
 export default function AccountPanel() {
-    
+
+
+return (
+    <Box>
+      <Center>
+      <Heading as="h3" size="lg" p={4}>
+        Account Panel
+      </Heading>
+      </Center>  
+      <Box borderWidth="1px" rounded="lg" overflow="hidden">
+        <Box overflowX="auto">
+          <Table size="sm">
+            <Thead>
+              <Tr>
+                <Th color="text">Username</Th>
+                <Th color="text">Password</Th>
+                <Th color="text">Level</Th>
+                <Th color="text">Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>{}</Tbody>
+          </Table>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
