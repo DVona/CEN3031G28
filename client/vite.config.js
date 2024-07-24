f0a760575c85a40b3e8557cef76a2ffe2ae33af8
Dominick Vona <dvona@ufl.edu>
2024-07-24 19:43:55 -0400

    reverting changes to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_API_URL ?? "http://localhost:5050"}`;
 
   return {
     plugins: [react()],
-    define: {
-      'process.env': {
-        VITE_API_URL: JSON.stringify(process.env.VITE_API_URL ?? "http://localhost:5050")
-      }
-    },  
   };
 });
