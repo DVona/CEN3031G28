aeb9ad57f0c9121e688f1476e7b8757dfc864df7
Justin-Toth <justintoth@ufl.edu>
2024-07-26 00:57:36 -0400

    Update for new feature (jwt, routes, middleware)

--- server/server.js
+++ server/server.js
@@ -1,24 +1,39 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 import connectDB from "./database/connection.js";
 import records from "./routes/record.js";
 import tickets from "./routes/ticketRoutes.js";
-import login from "./routes/login.js";
-import signup from "./routes/signup.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/login", login);
-app.use("/signup", signup);
+app.use(cookieParser());
 app.use("/record", records);
 app.use("/ticket", tickets);
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 connectDB();
 
 // start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+// middleware for error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
