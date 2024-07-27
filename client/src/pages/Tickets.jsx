2a02148eb68bb128fcec4e97563332bc70f600ab
Justin-Toth <justintoth@ufl.edu>
2024-07-26 20:40:32 -0400

    Remade Form and begun reimplementaiton of submission

--- client/src/pages/Tickets.jsx
+++ client/src/pages/Tickets.jsx
@@ -1,10 +1,10 @@
 import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
-import { VStack, } from "@chakra-ui/layout";
+import { VStack } from "@chakra-ui/layout";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useToast, Box, Flex, Heading, Text, Select } from "@chakra-ui/react";
+import { useToast, Box, Flex, Heading, Text, Textarea, Select } from "@chakra-ui/react";
 
 import axios from "axios";
 
@@ -12,16 +12,16 @@ export default function Tickets() {
   const toast = useToast();
   const navigate = useNavigate();
   const [selectedOption, setSelectedOption] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [description, setDescription] = useState("");
   const [loading, setLoading] = useState(false);
 
-  /*
+  // submission in progress (api route not implemented)
   const submitHandler = async (e) => {
     e.preventDefault();
+    console.log([selectedOption, description]);
 
     setLoading(true);
-    if (!username || !password || !confirmPassword) {
+    if (!selectedOption || !description) {
       toast({
         title: "Please Fill all the Feilds",
         status: "warning",
@@ -33,59 +33,44 @@ export default function Tickets() {
       return;
     }
 
-    if (password !== confirmPassword) {
-      toast({
-        title: "Passwords Do Not Match",
-        status: "warning",
-        duration: 2000,
-        isClosable: true,
-        position: "bottom",
-      });
-      setLoading(false);
-      return;
-    }
-
     try {
+      //dispatch(ticketSubmitStart());
       const config = {
         headers: {
           "Content-type": "application/json",
         },
       };
+      const res = await axios.post(`/api/user/ticket/${currentUser._id}`, { selectedOption, description }, config);
 
-      const res = await axios.post("/api/auth/signup", { username, password }, config);
       const data = res.data;
       if (data.success === false) {
         // error message?
         setLoading(false);
-        return;
+      } else {
+        toast({
+          title: "Ticket Submitted",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+
+        //dispatch(ticketSubmitSuccess(data));
       }
-
-      toast({
-        title: "Sign Up Successful",
-        description: "Please sign in to continue",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-
-      setLoading(false);
-      navigate("/");
     } catch (error) {
-      // this will usually occur because of duplicate usernames (other case would be form sanitization)
-      // cannot show actual error message as it will expose database information
       toast({
         title: "Error Occured!",
-        description: "Username is not allowed",
+        description: error.response.data.message,
         status: "error",
         duration: 5000,
         isClosable: true,
         position: "bottom",
       });
       setLoading(false);
+       //dispatch(ticketSubmitFailure(error.message));
     }
   };
-  */
+
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
       <Box borderWidth="1px" rounded="lg" p={5} width="55%" boxShadow="0 5px 10px 0 rgba(158, 158, 158, 0.75)">
@@ -98,7 +83,7 @@ export default function Tickets() {
           </Box>
           <FormControl id="Username" isRequired pt="4px">
             <FormLabel>Category</FormLabel>
-            <Select placeholder="Please Select an Option">
+            <Select placeholder="Please Select an Option" onChange={(e) => setSelectedOption(e.target.value)}>
               {/*
               {/*way to add the placeholder without making it an options (will throw error warning into console (can ignore))}
               <option selected hidden disabled value="">
@@ -112,21 +97,11 @@ export default function Tickets() {
               <option value="Other">Other</option>
             </Select>
           </FormControl>
-
-          {/* In Progress */}
-
-          <FormControl id="password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter password" />
+          <FormControl id="Description" isRequired>
+            <FormLabel>Description</FormLabel>
+            <Textarea height="20vh" width="100%" resize="none" placeholder="Please describe your issue" onChange={(e) => setDescription(e.target.value)} />
           </FormControl>
-          <FormControl id="Confirm Password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <Input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="confirmPassword" placeholder="Confirm Password" />
-          </FormControl>
-
-          {/* End of In Progress */}
-
-          <Button colorScheme="blue" mt={2} width="full" size="md" /*onClick={submitHandler}*/>
+          <Button colorScheme="blue" mt={2} width="full" size="md" onClick={submitHandler} isLoading={loading}>
             Submit
           </Button>
         </VStack>
