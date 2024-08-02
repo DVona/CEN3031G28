f7420b2710800e08d3b771742a173b20ecce2ee6
lucas <146390150+lucasrodr1010@users.noreply.github.com>
2024-08-02 00:26:33 -0400

    Allowing for larger profile picture sizes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./database/connection.js";
 import ticket from "./routes/ticketRoutes.js";
-
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
@@ -12,7 +11,9 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json()); Removing this because it for some reason limits payload size for web requests 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 app.use("/ticket", ticket);
 
